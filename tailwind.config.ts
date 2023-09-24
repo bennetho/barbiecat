1f6fc7e5ede119ee258e64312be96f917a3f93ce
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-09-24 11:07:14 +0500

    custom fonts set for the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        comfortaa: ["Comfortaa", "inter"],
+        carter: ["Carter One", "inter"],
+      },
+      colors: {
+        brand: "#419bfe",
+        white: {
+          main: "#FFFFFF",
+        },
+        black: {
+          main: "#000000",
+          off: "#0e0e0e",
+        },
+        yellow: {
+          main: "#ffa64b",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
