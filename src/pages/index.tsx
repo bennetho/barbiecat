519dea7bb0ad2639209701e09ad2abacb560469b
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-09-24 11:23:38 +0500

    landing page completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import Hero from "@/components/Hero/Hero";
+import Separator from "@/components/shared/Separator/Separator";
+import About from "@/components/About/About";
+import RoadMap from "@/components/RoadMap/RoadMap";
+import Tokenomics from "@/components/Tokenomics/Tokenomics";
+import Team from "@/components/Team/Team";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Separator />
+      <About />
+      <Separator />
+      <RoadMap />
+      <Separator />
+      <Tokenomics />
+      <Separator />
+      <Team />
+    </>
+  );
 }
